1946aceb539c33faddb2599025d67518ef97d06c
Isratosh <Isratosh@hotmail.com>
2015-11-13 13:50:38 -0700

    Uploading Link Class.

--- JavaTest.java
+++ JavaTest.java
@@ -1,19 +1,46 @@
-import java.util.Scanner;
-public class JavaTest {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello world!");
-		Scanner user_input = new Scanner( System.in );
-		String health;
-		System.out.println("Please input your health.");
-		health = user_input.next();
-		new HelloWorld(health);
-	}
-	public JavaTest(String health)
-	{
-		
-		System.out.println("You have " + health + " health. ayylmao");
-	}
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+/**
+ * Write a description of class Link here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class link extends Actor
+{
+    protected static final String FILE_IMAGE_LOCATION = "images/link";
+    public int health;
+    public boolean hasSword;
+    public boolean hasBoomerang;
+    public GreenfootImage icon; 
+    public int bombs;
+    public String facingDirection;
+    public String movementState;
+    public link(int health)
+    {
+        this.health = health;
+        draw();
+    }
+    public void draw()
+    {
+        String fileName = FILE_IMAGE_LOCATION;
+        facingDirection = "south"; //TEMPORARY
+        movementState = "notwalking";
+        fileName += "/" + facingDirection;
+        fileName += "/" + movementState;
+        fileName += "/link.png";
+        fileName = fileName.toLowerCase();
+        //fileName = "link.png";
+        setImage(fileName);
+        System.out.println(fileName); //DEBUG
+    }
+    /**
+     * Act - do whatever the Link wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        
+    }    
 }
+
