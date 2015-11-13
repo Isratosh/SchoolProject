1a9f7ce290f64af8985d78d9963628c19f406a1e
Isratosh <Isratosh@hotmail.com>
2015-11-13 13:51:33 -0700

    Uploading Object Class.

--- LinkProject/test.java
+++ LinkProject/test.java
@@ -1,15 +1,27 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class test here.
+ * Write a description of class object here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class test extends Actor
+public class object extends Actor
 {
+    public object(String icon)
+    {
+        String fileName = "images/";
+        fileName += icon;
+        fileName += ".png";
+        fileName = fileName.toLowerCase();
+        setImage(fileName);
+    }
+    public void removeSelf()
+    {
+        getWorld().removeObject(this);
+    }
     /**
-     * Act - do whatever the test wants to do. This method is called whenever
+     * Act - do whatever the object wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
