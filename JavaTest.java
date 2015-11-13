e5aed41e22bc18806e47a9af2ce90c1d3f6c56c4
Isratosh <Isratosh@hotmail.com>
2015-11-12 23:15:06 -0700

    Teeeeest

---
+++ JavaTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class JavaTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+		Scanner user_input = new Scanner( System.in );
+		String health;
+		System.out.println("Please input your health.");
+		health = user_input.next();
+		new HelloWorld(health);
+	}
+	public JavaTest(String health)
+	{
+		
+		System.out.println("You have " + health + " health.");
+	}
+
+}
